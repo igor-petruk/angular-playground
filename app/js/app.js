c0e72d263f62f6b85857257406eb2d0086149dd7
Igor Petruk <igor.petrouk@gmail.com>
2013-03-22 01:44:39 +0200

    Initial import

---
+++ app/js/app.js
@@ -0,0 +1,19 @@
+function TodoController($scope){
+    $scope.todos = [
+        {text:"Great", done:false}
+    ];
+    $scope.totalTodos = function(){
+        return $scope.todos.length;
+    };
+
+    $scope.addTodo = function(){
+        $scope.todos.push({text:$scope.formTodoText, done:false});
+        $scope.formTodoText = ""
+    };
+
+    $scope.clearCompleted = function(){
+        $scope.todos = _.filter($scope.todos,function(item){
+            return !item.done;
+        });
+    };
+};
